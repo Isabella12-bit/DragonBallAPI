516a15bbe78d54b61a0b1737ca4a0c3814faa135
Isabella-12bit <isabella.montes@ucb.edu.bo>
2025-06-04 10:12:14 -0400

    Fix: Arreglo del navbar y agregado de un home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PersonajesComponent } from './paginas/personajes/personajes.component';
 import { DetallePersonajeComponent } from './paginas/detalle-personaje/detalle-personaje.component';
 import { LoginComponent } from './paginas/login/login.component';
+import { HomeComponent } from './paginas/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'personajes', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'personajes', component: PersonajesComponent },
   { path: 'login', component: LoginComponent },
-   { path: 'detalle/:id', component: DetallePersonajeComponent }
+  { path: 'detalle/:id', component: DetallePersonajeComponent }
 ];
